c45e125edd72ffc0bb4be0b2f1a3e7328a5cdbdc
buchniy <hhccuubb@ukr.net>
2020-06-05 16:32:35 +0300

    wh8 cays2

--- wh8/js/wh8.2.js
+++ wh8/js/wh8.2.js
@@ -21,25 +21,29 @@
  */
 console.log('===================================');
 data = {
-    addRecord: function({}, flag){
+    addRecord: function () {
         console.log(arguments, 'arguments');
-        for(var i = 0; i < arguments.length; i++){
-            // console.log(arguments[i]);
-            // this.arguments[i];
+
+        for (var i = 0; i < arguments.length; i++) {
+            for (let key in arguments[i]) {
+
+                if (arguments[arguments.length - 1] === true) {
+
+                    if (!(this.hasOwnProperty(key))) {
+                        this[key] = arguments[i][key];
+                    }
+                }else{
+                    this[key] = arguments[i][key];
+                }
+            }
         }
     },
+    x: 'изначальный',
     p: 600,
     str: 'hello',
     y: -50
 };
-data.addRecord({x: 10}, {y: 20}, {z: 30, x: 50}, false);
-data.x  = 50;
-data.y // 20
-data.z // 30
-data.p // 600
-data.str // 'hello'
-
-
-for(let key in data){
+data.addRecord({x: 'повторный'}, {y: 20}, {z: 30, f: 50});
+data.addRecord({z: 50}, {j: 6000}, {x: 'третий вызов'}, true);
 console.log(data, 'data');
-};
+
