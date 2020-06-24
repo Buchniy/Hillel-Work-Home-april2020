b60df54ca11ac31296a8d5bb0c70f615c40fcd4b
buchniy <hhccuubb@ukr.net>
2020-06-24 17:30:42 +0300

    case1

--- Chernovik/JS/cernovik.js
+++ Chernovik/JS/cernovik.js
@@ -1,7 +1,8 @@
 /*
 Создаем 2 блока, с кнопкой Нажмите и счетчиком счетчик (в виде числа).
 При нажатии на Click - счетчик увеличивается. При перезагрузке страницы counter
-Доп задание 2) Создать кнопку ClearCounters () - очищает все счетчики
+Доп задание
+2) Создать кнопку ClearCounters () - очищает все счетчики
 
 Создать кнопку setCounter (), который запрашивает идентификатор
 (в каком блоке изменить счетчик).
@@ -9,6 +10,7 @@
 console.log('=================WH16================');
 window.onload = function() {
     var buttons = document.querySelectorAll('.button');
+    var buttotClear = document.querySelector('.button1');
 
     var store = {
         setInStore: function(key, value) {
@@ -17,8 +19,11 @@ window.onload = function() {
         getFromStore: function(key) {
             return localStorage.getItem(key);
         }
+        // clearStorage: function () {
+        //     localStorage.clear();
+        // }
     };
-
+    buttotClear.addEventListener('click', localStorage.clear());
     for(var i = 0; i < buttons.length; i++) {
         getParent(buttons[i],'.block')
             .querySelector('.counter').innerHTML = store.getFromStore('counter-' + i);
@@ -26,7 +31,7 @@ window.onload = function() {
         buttons[i].addEventListener('click', click.bind(buttons[i], i));
 
     }
-
+    // buttotClear.addEventListener('click', localStorage.clearStorage());
 
     function click(i) {
         var block  = getParent(this,'.block');
@@ -40,5 +45,6 @@ window.onload = function() {
     function getParent(node, parentSelector) {
         return node.closest(parentSelector);
     }
+
 
 };
