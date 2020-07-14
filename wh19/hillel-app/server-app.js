4d182cedac5960333e63e0f11d4df12a380cca07
buchniy <hhccuubb@ukr.net>
2020-07-14 14:31:57 +0300

    wh19 case3

--- wh19/hillel-app/server-app.js
+++ wh19/hillel-app/server-app.js
@@ -22,77 +22,32 @@ app.listen(port, function () {
     console.log(`Example app listening on port http://localhost:${port}/`);
 });
 // http://localhost:3003/
-////--------------------------/////
 
-
-
-
-// app.post('/', function(req, res) {
-//     console.log('hello path / -->> post./users.json', req.body);
-//     const {login , secretKey} = req.body;
-//     const data2 = req.body;
-//     res.send(data2);
-// });
-//
-
-// fs.readFile('./users.json', 'utf8', (err, jsonString) => {
-//     if (err) {
-//         console.log("File read failed:", err);
-//         return
-//     }
-//     let fileData = jsonString;
-//
-//     console.log('File data:', fileData);
-//
-// fileData.order_count += 1;
-// fs.writeFile('./users.json', JSON.stringify(jsonString), (err) => {
-//     if (err) console.log('Error writing file:', err)
-// });
-// });
-
-app.post('/', function(req, res) {
-    // console.log('body', req.body);
+app.post('/', function (req, res) {
     let {login, secretKey} = req.body;
     let data = req.body;
-    // res.send(JSON.stringify(data));
 
-
-
-// ////////////////////////////////////////////////////////////////
     fs.readFile('./users.json', 'utf8', (err, dataJson) => {
         if (err) {
             console.log("File read failed:", err);
             return
         }
         let data2 = JSON.parse(dataJson);
-        let newUser = data2.some(data2 => data2.login === login && data2.secretKey === secretKey);
-        // console.log('newUser', newUser);
-        // console.log('data2', data2);
+        let currentUser = data2.some(data2 => data2.login === login && data2.secretKey === secretKey);
 
-
-        if(newUser){
-            console.log('user present');
-            let er = 'error';
-            res.status(301).send(er);
-        } else{
+        if (currentUser) {
+            let message = `Пользователь "${login}" с таким secretKey уже существует`;
+            res.status(301).send(message);
+        } else {
             data2.push(data);
-            let ok = "ok";
-            res.send(ok).status(200);
-            console.log('data2', data2);
+            message = `Пользователь "${login}" успешно добавлен`;
+            res.status(200).send(message);
         }
 
-
-
-
-        fs.writeFile('./users.json', data2, (err) => {
+        fs.writeFile('./users.json', JSON.stringify(data2), (err) => {
 
             if (err) console.log('Error writing file:', err)
         });
 
-    /////////////////////////////////////////////////////////////////
-
-
     });
 });
-
-// node server-app
