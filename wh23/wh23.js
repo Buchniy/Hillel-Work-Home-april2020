b236f77fb8c5e450b968aaeaf705b87e52046829
buchniy <hhccuubb@ukr.net>
2020-07-20 17:28:54 +0300

    hw23

--- wh23/wh23.js
+++ wh23/wh23.js
@@ -1,13 +1,79 @@
-console.log('Vue', Vue);
+console.log(Vue, 'Vue');
+
 const data = {
-    a: 1,
-    b: 2,
-    sum() {
-        return this.a + this.b + ' HEllo world';
+    bucketList1: [
+        {
+            text: 'Дело1'
+        },
+        {
+            text: 'Дело2'
+        },
+        {
+            text: 'Дело3'
+        },
+        {
+            text: 'Дело4'
+        },
+        {
+            text: 'Дело5'
+        },
+        {
+            text: 'Дело6'
+        }
+    ],
+    bucketList2: [
+        {
+            text: 'Другое Дело1'
+        },
+        {
+            text: 'Другое Дело2'
+        },
+        {
+            text: 'Другое Дело3'
+        },
+        {
+            text: 'Другое Дело4'
+        },
+        {
+            text: 'Другое Дело5'
+        },
+        {
+            text: 'Другое Дело6'
+        }
+    ],
+    // addMenuItem(){
+    //  bucketList.shift()
+    //
+    // },
+    onClick1() {
+        console.log(this.bucketList1, 'bucketList1');
+        let qqq = this.bucketList1.pop();
+        console.log(qqq, 'qqq');
+        this.bucketList2.unshift(qqq)
     },
+    onClick2() {
+        console.log(this.bucketList2, 'bucketList2');
+        let www = this.bucketList2.pop();
+        this.bucketList2.pop();
+        this.bucketList1.unshift(www)
+    }
+
+};
 
 
-let vue = new Vue({
+
+//     onClick(value) {
+//         console.log(this.menuSecond, 'menuSecond');
+//     },
+//     data: {
+//         text: ''
+//     }
+// };
+
+const config = {
     el: '#app',
     data: data
-}),
+};
+
+const vue = new Vue(config);
+
