ccd416aa3b56d7fbdf83ab8faa3bbd3898eb3c87
Buch <hhccuubb@ukr.net>
2020-07-20 22:19:30 +0300

    wh23 case1

--- wh23/wh23.js
+++ wh23/wh23.js
@@ -3,53 +3,53 @@ console.log(Vue, 'Vue');
 const data = {
     bucketList1: [
         {
-            text: 'Дело1'
+            text: 'Дело 1'
         },
         {
-            text: 'Дело2'
+            text: 'Дело 2'
         },
         {
-            text: 'Дело3'
+            text: 'Дело 3'
         },
         {
-            text: 'Дело4'
+            text: 'Дело 4'
         },
         {
-            text: 'Дело5'
+            text: 'Дело 5'
         },
         {
-            text: 'Дело6'
+            text: 'Дело 6'
         }
     ],
     bucketList2: [
         {
-            text: 'Другое Дело1'
+            text: 'Другое дело 1'
         },
         {
-            text: 'Другое Дело2'
+            text: 'Другое дело 2'
         },
         {
-            text: 'Другое Дело3'
+            text: 'Другое дело 3'
         },
         {
-            text: 'Другое Дело4'
+            text: 'Другое дело 4'
         },
         {
-            text: 'Другое Дело5'
+            text: 'Другое дело 5'
         },
         {
-            text: 'Другое Дело6'
+            text: 'Другое дело 6'
         }
     ],
     onClick1() {
         console.log(this.bucketList1, 'bucketList1');
-        let qqq = this.bucketList1.pop();
-        this.bucketList2.unshift(qqq)
+        let list1 = this.bucketList1.pop();
+        this.bucketList2.unshift(list1)
     },
     onClick2() {
         console.log(this.bucketList2, 'bucketList2');
-        let www = this.bucketList2.pop();
-        this.bucketList1.unshift(www)
+        let list2 = this.bucketList2.pop();
+        this.bucketList1.unshift(list2)
     }
 
 };
