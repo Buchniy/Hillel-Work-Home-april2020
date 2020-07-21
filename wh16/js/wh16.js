fafe4455b91e1da0394f0d858f799ccdfd1208fc
buchniy <hhccuubb@ukr.net>
2020-07-21 16:42:27 +0300

    hw16

--- wh16/js/wh16.js
+++ wh16/js/wh16.js
@@ -1,62 +1,75 @@
-/*
-Создать кнопку ClearCounters() - очищает все счетчики
+console.log('=================WH16/17================');
+window.onload = function () {
+    let buttons = document.querySelectorAll('.button');
+    let blocks = document.querySelectorAll('.block');
+    let buttonClear = document.querySelector('.button1');
+    let setCounter = document.querySelector('.button2');
 
-Создать кнопку setCounter(), который запрашивает id (каком блоку изменить счетчик)
- */
-console.log('=================WH16================');
-window.onload = function() {
-    var buttons = document.querySelectorAll('.button');
-    var buttonClear = document.querySelector('.button1');
-    var setCounter = document.querySelector('.button2');
-
-    var store = {
-        setInStore: function(key, value) {
+    let store = {
+        setInStore: function (key, value) {
             localStorage.setItem(key, value);
         },
-        getFromStore: function(key) {
+        getFromStore: function (key) {
             return localStorage.getItem(key);
         }
 
     };
 
-    for(var i = 0; i < buttons.length; i++) {
-        getParent(buttons[i],'.block')
+    for (let i = 0; i < buttons.length; i++) {
+        getParent(buttons[i], '.block')
             .querySelector('.counter').innerHTML = store.getFromStore('counter-' + i);
 
         buttons[i].addEventListener('click', click.bind(buttons[i], i));
         buttonClear.addEventListener('click', clearKey.bind(null, i));
-        setCounter.addEventListener('click', set);
+        setCounter.addEventListener('click', setCounterKey);
     }
 
     function click(i) {
-        var block  = getParent(this,'.block');
-        var counter = block.querySelector('.counter');
-        var counterValue = ++counter.innerHTML;
+        let block = getParent(this, '.block');
+        let counter = block.querySelector('.counter');
+        let counterValue = ++counter.innerHTML;
 
         store.setInStore('counter-' + i, counterValue); // counter - 2
     }
 
     function getParent(node, parentSelector) {
-        return node.closest(parentSelector);
+        return node.closest(parentSelector)
     }
 
     function clearKey(i) {
         store.setInStore('counter-' + i, 0);
-
+        return synchronize(i);
     }
 
-    function set() {
-        var keyBox = prompt('какому ключу изменить счетчик', 'counter-');
-        var keys = Object.keys(localStorage);
-        for(var key of keys) {
-            if(key === keyBox){
-                var setValue = +prompt('новое числовое значение', '...');
-                if(!isNaN(setValue)){
-                    store.setInStore(keyBox, setValue);
-                }else{
-                    alert('значенние должно быть числом')
-                }
+
+    blocks.forEach((item, index) =>
+        blocks[index].setAttribute('data-id', `counter-${index}`));
+
+
+    function setCounterKey() {
+        let keyBox = prompt('какому ключу изменить счетчик', 'counter-');
+        blocks.forEach((item, index) => {
+
+            if (blocks[index].dataset.id === keyBox) {
+                return setCounterValue(keyBox, index)
             }
+        });
+    }
+
+    function setCounterValue(keyBox, index) {
+        let setValue = +prompt('новое числовое значение', '...');
+        if (!isNaN(setValue)) {
+            store.setInStore(keyBox, setValue);
+            return synchronize(index)
+
+        } else {
+            alert('значенние должно быть числом')
         }
     }
+
+    function synchronize(index) {
+        getParent(blocks[index], '.block')
+            .querySelector('.counter').innerHTML = store.getFromStore('counter-' + index);
+    }
+
 };
