ee3462eafd77484943a7bc59845192eb256b4f22
buchniy <hhccuubb@ukr.net>
2020-07-23 17:20:16 +0300

    wh22

--- wh22/client/main.js
+++ wh22/client/main.js
@@ -4,118 +4,35 @@ window.onload = function(){
 
 
 let country = document.querySelector('#country');
-    const select = document.querySelector('select');
-    select.addEventListener('click', click);
+let select = document.querySelector('select');
 
 async function modernSendAjax() {
     const data = await fetch('http://localhost:3005/freez');
-    const resp = await data.json();
-    console.log(resp, 'res');
-    country.innerHTML = countrySelection(resp);
-    .then()
+    const respCountry = await data.json();
+    console.log(respCountry, 'res');
+    country.innerHTML = countrySelection(respCountry);
+    selection();
+    // return await selection();
+    // resolve(selection());
 }
 
-modernSendAjax();
-
-
+function selection() {
+    select.addEventListener("change", function() {
+        let currentCountry = this.value;
+        console.log(currentCountry, 'currentCountry');
+        return currentCountry;
+    });
+}
 function countrySelection(resp) {
     return `${resp.map(index => `<option value="${index}">${index} </option>`).join(' ')}`
 }
-    function click(event) {
-        // event.preventDefault();
-        if()
-        var a=document.querySelector('#country').value;
-        // const target = event.target;
-        console.log(a, 'a');
-    }
-    // function getMarkup(arr) {
-    //     return `<div class="goods">
-    //         ${
-    //         arr
-    //             .map(good => `<div class="good ${good.isAvailability? 'ok': 'absent'}">${good.name} - ${good.price}</div>`)
-    //             .join(' ')
-    //     }
-    //     </div> `;
 
-    //
-    // `<select size="1" id="country">
-    //             ${resp.forEach((item, index) =>
-    //     `<option >
-    //                      ${resp[index]}
-    //                  </option>`) }
-    //         </select>` ;
 
-    //`<option>${resp[index]}</option>`
 
-    // `${resp.forEach((item, index) =>
-    //                 `<option >
-    //                      ${resp[index]}
-    //                  </option>`) }
-    //        ` ;
+    modernSendAjax()
+        .then((resp) => console.log(resp, 'currentCountry'));
 
 
 
 
-// const xhttp = new XMLHttpRequest();
-//
-// xhttp.addEventListener('load', function ({target}) {
-//     const response = target.response;
-//     success(response);
-//
-// });
-//
-// xhttp.open(method, url);
-// xhttp.send()
-
-
-// const form = document.querySelector('form');
-// form.addEventListener('click', click);
-
-// function click(event) {
-//     event.preventDefault();
-//     const target = event.target;
-//
-//     if (target.tagName.toLowerCase() !== 'button') {
-//         return;
-//     }
-//     const requestPayload = prepareForm(this);
-//     sendAjax(
-//         {
-//         url: `http://localhost:3005/freez`,
-//         method: 'POST',
-//         // data: requestPayload,
-//             success: function () {
-//             }
-//         });
-
-
-
-
-//
-// function prepareForm(form) {
-//     const requestPayload = {};
-//
-//     [].forEach.call(form, function(item) {
-//         const isNotButton = item.tagName.toLowerCase() !== 'button';
-//
-//         if (isNotButton) {
-//             requestPayload[item.name] = item.value;
-//         }
-//     });
-//
-//     return requestPayload;
-// }
-// function getMarkup(arr) {
-//     return `<div class="goods">
-//             ${
-//         arr
-//             .map(good => `<div class="good ${good.isAvailability? 'ok': 'absent'}">${good.name} - ${good.price}</div>`)
-//             .join(' ')
-//     }
-//         </div> `;
-// }
-// function clear() {
-//     const box =  document.querySelector('.box');
-//     box.innerHTML = "User Not found";
-// }
-}
+};
