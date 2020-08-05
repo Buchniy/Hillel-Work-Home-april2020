187df8abe1c831e043b3eae09919ace4cdcfa2fa
buchniy <hhccuubb@ukr.net>
2020-08-05 15:53:31 +0300

    wh24

--- wh24/components.js
+++ wh24/components.js
@@ -1,66 +1,132 @@
-Vue.component('navigation', {
+// Vue.component('navigation', {
+//     data() {
+//         return {
+//             user: 'Valera',
+//             cssClassList: ['navigation', 'header-navigation'],
+//             navItems: ['Main', 'Contacts', 'Gallery'],
+//             titleFromChild: 'default'
+//         }
+//     },
+//     methods: {
+//         helloEvent(targetFromChild, b){
+//             console.log('helloEvent', targetFromChild, b)
+//         },
+//         getSendValue(title) {
+//             console.log(title + ' title ===> in parent');
+//             this.titleFromChild = title;
+//         }
+//     },
+//     template: `
+//         <header :class="cssClassList">
+//             <navigation-item
+//                 @hello="helloEvent"
+//                 @send="getSendValue"
+//                 :navTitle="item"
+//                 v-for="item in navItems" />
+//             <hr />
+//             <selected-page :pageTitle="titleFromChild"/>
+//         </header>
+//     `
+// });
+// /////////////////////////////////////////////////////
+// Vue.component('navigation-item', {
+//     props: ['navTitle'],
+//     data() {
+//         return {
+//
+//         }
+//     },
+//     methods: {
+//         onClick(event) {
+//             // this.$emit('hello', event.target, 2);
+//             this.$emit('send', this.navTitle);
+//             // console.log(this.navTitle, 'navTitle');
+//         }
+//     },
+//     template: `
+//         <button
+//             @click="onClick"
+//             class="navigation-item"
+//             >{{navTitle}}</button>
+//     `
+// });
+// ////////////////////////////////////////////////////////////
+// Vue.component('selected-page', {
+//     props: ['pageTitle'],
+//     template: `
+//         <div>Selected ->> {{pageTitle}}</div>
+//     `
+// });
+Vue.component('Diagram', {
     data() {
         return {
-            user: 'Valera',
-            cssClassList: ['navigation', 'header-navigation'],
-            navItems: ['Main', 'Contacts', 'Gallery'],
-            titleFromChild: 'default'
+            diagramItems:
+                [
+                    { 'name': 'col-1', 'color': 'red'},
+                    { 'name': 'col-2', 'color': 'lightblue'},
+                    { 'name': 'col-3', 'color': 'yellow'},
+                    { 'name': 'col-4', 'color': 'lightgreen'},
+                    { 'name': 'col-5', 'color': 'fuchsia'},
+                    { 'name': 'col-6', 'color': 'darkgrey'},
+                    { 'name': 'col-7', 'color': 'pink'}
+                ]
         }
     },
     methods: {
-        helloEvent(targetFromChild, b){
-            console.log('helloEvent', targetFromChild, b)
-        },
-        getSendValue(title) {
-            console.log(title + ' title ===> in parent');
-            this.titleFromChild = title;
-        }
+       kuku() {
+           console.log('kuku')
+       }
     },
     template: `
-        <header :class="cssClassList">
-            <navigation-item 
-                @hello="helloEvent"
-                @send="getSendValue"
-                :navTitle="item" 
-                v-for="item in navItems" />
-            <hr />
-            <selected-page :pageTitle="titleFromChild"/> 
-        </header>
+    <div class="parent-box">
+    <diagram-column 
+        
+        :column="item"
+        v-for="item in diagramItems" />
+    </div>        
     `
 });
-
-Vue.component('navigation-item', {
-    props: ['navTitle'],
+//----------------------------------------///
+Vue.component('diagram-column', {
+    props: ['column'],
     data() {
         return {
 
         }
     },
     methods: {
-        onClick(event) {
-            // this.$emit('hello', event.target, 2);
-            this.$emit('send', this.navTitle);
-            // console.log(this.navTitle, 'navTitle');
-        }
+
+
     },
     template: `
-        <button
-            @click="onClick"
-            class="navigation-item"
-            >{{navTitle}}</button>
+        <div class="box" >
+            <div class="" :style="{background: column.color, height: '100px'}"></div>
+            <div>{{column.name}}</div>
+            <regulator />
+        </div>
     `
 });
+//=============================================//
+Vue.component('regulator', {
+    // props: ['regul'],
+    data() {
+        return {
 
-Vue.component('selected-page', {
-    props: ['pageTitle'],
+        }
+    },
+    methods: {
+
+    },
     template: `
-        <div>Selected ->> {{pageTitle}}</div>
+     <input type="range">
+<!--        <div :style="{background: column.color} ">{{column.name}}</div>-->
     `
 });
 
+//-----------------------------------------------------------//
 const vue = new Vue({
     el: '#app',
     data: {
-        foo: 1
+
     }
 });
