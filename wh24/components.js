23b1e5383c8010100d815a5fad04a0a6a21b46ed
buchniy <hhccuubb@ukr.net>
2020-08-06 17:02:42 +0300

    wh24

--- wh24/components.js
+++ wh24/components.js
@@ -1,113 +1,59 @@
-// Vue.component('navigation', {
-//     data() {
-//         return {
-//             user: 'Valera',
-//             cssClassList: ['navigation', 'header-navigation'],
-//             navItems: ['Main', 'Contacts', 'Gallery'],
-//             titleFromChild: 'default'
-//         }
-//     },
-//     methods: {
-//         helloEvent(targetFromChild, b){
-//             console.log('helloEvent', targetFromChild, b)
-//         },
-//         getSendValue(title) {
-//             console.log(title + ' title ===> in parent');
-//             this.titleFromChild = title;
-//         }
-//     },
-//     template: `
-//         <header :class="cssClassList">
-//             <navigation-item
-//                 @hello="helloEvent"
-//                 @send="getSendValue"
-//                 :navTitle="item"
-//                 v-for="item in navItems" />
-//             <hr />
-//             <selected-page :pageTitle="titleFromChild"/>
-//         </header>
-//     `
-// });
-// /////////////////////////////////////////////////////
-// Vue.component('navigation-item', {
-//     props: ['navTitle'],
-//     data() {
-//         return {
-//
-//         }
-//     },
-//     methods: {
-//         onClick(event) {
-//             // this.$emit('hello', event.target, 2);
-//             this.$emit('send', this.navTitle);
-//             // console.log(this.navTitle, 'navTitle');
-//         }
-//     },
-//     template: `
-//         <button
-//             @click="onClick"
-//             class="navigation-item"
-//             >{{navTitle}}</button>
-//     `
-// });
-// ////////////////////////////////////////////////////////////
-// Vue.component('selected-page', {
-//     props: ['pageTitle'],
-//     template: `
-//         <div>Selected ->> {{pageTitle}}</div>
-//     `
-// });
 Vue.component('Diagram', {
     data() {
         return {
             diagramItems:
                 [
-                    { 'name': 'col-1', 'color': 'red'},
-                    { 'name': 'col-2', 'color': 'lightblue'},
-                    { 'name': 'col-3', 'color': 'yellow'},
-                    { 'name': 'col-4', 'color': 'lightgreen'},
-                    { 'name': 'col-5', 'color': 'fuchsia'},
-                    { 'name': 'col-6', 'color': 'darkgrey'},
-                    { 'name': 'col-7', 'color': 'pink'}
+                    {'name': 'col-1', 'color': 'red'},
+                    {'name': 'col-2', 'color': 'lightblue'},
+                    {'name': 'col-3', 'color': 'yellow'},
+                    {'name': 'col-4', 'color': 'lightgreen'},
+                    {'name': 'col-5', 'color': 'fuchsia'},
+                    {'name': 'col-6', 'color': 'darkgrey'},
+                    {'name': 'col-7', 'color': 'pink'}
                 ]
         }
     },
-    methods: {
-       kuku() {
-           console.log('kuku')
-       }
-    },
+    methods: {},
     template: `
+<div>
     <div class="parent-box">
+
     <diagram-column 
-        
         :currentColumn="item"
         v-for="item in diagramItems" />
-    </div>        
+  
+    </div> 
+    <assort />
+</div>
+    
     `
 });
-//----------------------------------------///
+//------------------column----------------------//
 Vue.component('diagram-column', {
     props: ['currentColumn'],
     data() {
         return {
-            valueHeight:''
+            valueHeight: ''
         }
     },
+    mounted() {
+        this.valueHeight = localStorage.getItem(this.currentColumn.name)
+        console.log(this.currentColumn.name, this.valueHeight);
+    },
     methods: {
-        changeValueHeight(value){
-            this.valueHeight = value;
+        changeValueHeight(currentValue) {
+            this.valueHeight = currentValue;
         }
     },
     template: `
         <div class="box" >
             <div class="color-box" :style="{background: currentColumn.color, height: valueHeight + 'px'}"></div>
             <div>{{currentColumn.name}}</div>
-            <regulator :valueRange="currentColumn" @changeValue="changeValueHeight"/>
+            <regulator :valueRange="currentColumn.name" @changeValue="changeValueHeight"/>
         </div>
     `
 });
-//=============================================//
+//------------------regulator----------------------//
 Vue.component('regulator', {
     props: ['valueRange'],
     data() {
@@ -115,13 +61,20 @@ Vue.component('regulator', {
             value: 0,
         }
     },
-    methods: {
-        onClick(event){
-            this.$emit('changeValue', this.value);
-            console.log('this.value', this.value)
+    mounted() {
 
+        let getStor = localStorage.getItem(this.valueRange);
+        if (getStor) {
+            this.value = getStor;
+        } else {
+            this.value = 0;
         }
-
+    },
+    methods: {
+        onClick(event) {
+            this.$emit('changeValue', this.value);
+            localStorage.setItem(this.valueRange, this.value)
+        }
     },
     template: `
         <div>
@@ -132,10 +85,31 @@ Vue.component('regulator', {
     `
 });
 
-//-----------------------------------------------------------//
+//------------------assort ----------------------//
+Vue.component('assort', {
+    props: ['valueFilter'],
+    data() {
+        return {
+
+        }
+    },
+    mounted() {
+
+    },
+    methods: {
+onClick(event) {
+    // console.log(this.valueFilter, 'valueFilter');
+}
+    },
+    template: `
+        <div class="assort">
+            <button @click="onClick" class="btn">filter</button>
+        </div>
+    `
+});
+
+// //----------------------------------------//
 const vue = new Vue({
     el: '#app',
-    data: {
-
-    }
+    data: {}
 });
