2cc72374cc0303a8372438c29239e00b534b3d68
buchniy <hhccuubb@ukr.net>
2020-08-14 17:23:21 +0300

    work22

--- wh22/client/main.js
+++ wh22/client/main.js
@@ -1,83 +1,63 @@
 
 window.onload = function(){
 
-
-
 let country = document.querySelector('#country');
 let city = document.querySelector('#city');
 let street = document.querySelector('#street');
-
+//----------------------------------//
 async function startSendAjax() {
-    const data = await fetch('http://localhost:3005/');
-    const respCountry = await data.json();
-    // console.log(respCountry, 'res');
-    country.innerHTML = countrySelection(respCountry);
-    return data
+    return  await axios.get('http://localhost:3005/')
+        .then(response =>country.innerHTML = countrySelection(response.data))
+        .catch(function (error) {onsole.log(error);});
 }
-///////////////////
-    async function nextAjax(event) {
+//----------------------------------//
+
+async function nextAjax(event) {
         let val = event.target.value;
         console.log(val, 'country');
-        const firstRequest = axios.get(`http://localhost:3005/${val}/?street=${val}`)
-                .then(response => city.innerHTML = countrySelection(response.data));
-        return firstRequest
-        }
-    async function streetAjax(event){
-             let str = event.target.value;
-             console.log(str, 'city');
-             const secondRequest = axios.get(`http://localhost:3005/street/${str}/?str=${str}`)
-                     .then(response=>street.innerHTML = countrySelection(response.data));
-    // .then(response=>console.log(response, 'response.data'));
-        return secondRequest
-            }
 
-/////////////////////////////////////
-//     async function nextAjax(event) {
-//     let val = event.target.value;
-//         console.log(val, 'country');
-//         const data = await fetch(`http://localhost:3005/${val}/?street=${val}`);
-//         const respCity = await data.json();
-//         // console.log(respCountry, 'res');
-//         city.innerHTML = countrySelection(respCity);
-//         return data
-//
-//     }
-/////////////////////////
-//     async function streetAjax(event) {
-//         let str = event.target.value;
-//         console.log(str, 'city');
-//         const data = await fetch(`http://localhost:3005/street/${str}/?str=${str}`);
-//         const respStreet = await data.json();
-//
-//         street.innerHTML = countrySelection(respStreet);
-//
-//         // console.log(respStreet, 'currentstr');
-//         return data
-//     }
+    return await axios.get(`http://localhost:3005/${val}/?street=${val}`)
+            .then(response => city.innerHTML = countrySelection(response.data))
+            .catch(function (error) {console.log(error);});
+}
+//----------------------------------//
 
-        function currentTarget(event) {
-            let currentTarget = event.target.value;
-            console.log(currentTarget, 'street');
-        }
+async function streetAjax(event){
+        let str = event.target.value;
+        console.log(str, 'city');
 
-        function countrySelection(resp) {
-            return `${resp.map(index => `<option value="${index}">${index} </option>`).join(' ')}`
-        }
+    return  await axios.get(`http://localhost:3005/street/${str}/?str=${str}`)
+            .then(response=>street.innerHTML = countrySelection(response.data))
+            .catch(function (error) {console.log(error); });
+}
+//----------------------------------//
+function currentTarget(event) {
+       let currentTarget = event.target.value;
+       console.log(currentTarget, 'street');
+       return currentTarget
+}
+//----------------------------------//
+function countrySelection(resp) {
+    return `${resp.map(index => `<option  value="${index}">${index} </option>`).join(' ')}`
+ }
+//----------------------------------//
+startSendAjax();
+// country.addEventListener("change", nextAjax);
+// city.addEventListener("change", streetAjax);
+// street.addEventListener("change", currentTarget);
+//----------------------------------//
+Promise.all(
+   [
 
-
-        startSendAjax();
-        country.addEventListener("change", nextAjax);
-        city.addEventListener("change", streetAjax);
-        street.addEventListener("change", currentTarget);
-
-        // Promise.all([startSendAjax(), country.addEventListener("change", nextAjax), streetAjax]).then((data) => {
-        //     console.log(data, 'data resolved');
-        // });
-
-        // Promise.all([startSendAjax(), secondRequest]).then((value)=>
-        //   console.log(value, 'results'));
+            country.addEventListener("change", nextAjax),
+          city.addEventListener("change", streetAjax),
+           street.addEventListener("change", currentTarget)
+   ])
+           .then(results=> console.log(results, 'results'))
+           .catch(function (error) {console.log(error)});
 
 };
+
 //////////////ковырялка///////////////////////////////////////
 
 // window.onload = function(){
@@ -145,3 +125,26 @@ async function startSendAjax() {
 //     //   console.log(value, 'results'));
 
 // };
+/////////////////////////////////////
+//     async function nextAjax(event) {
+//     let val = event.target.value;
+//         console.log(val, 'country');
+//         const data = await fetch(`http://localhost:3005/${val}/?street=${val}`);
+//         const respCity = await data.json();
+//         // console.log(respCountry, 'res');
+//         city.innerHTML = countrySelection(respCity);
+//         return data
+//
+//     }
+/////////////////////////
+//     async function streetAjax(event) {
+//         let str = event.target.value;
+//         console.log(str, 'city');
+//         const data = await fetch(`http://localhost:3005/street/${str}/?str=${str}`);
+//         const respStreet = await data.json();
+//
+//         street.innerHTML = countrySelection(respStreet);
+//
+//         // console.log(respStreet, 'currentstr');
+//         return data
+//     }
