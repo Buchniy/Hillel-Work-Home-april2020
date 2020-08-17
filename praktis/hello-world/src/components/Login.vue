40e6b7b95edc0481dd2f262e0bf29787d8f41a66
buchniy <hhccuubb@ukr.net>
2020-08-17 12:29:03 +0300

    17/08

--- praktis/hello-world/src/components/Login.vue
+++ praktis/hello-world/src/components/Login.vue
@@ -2,13 +2,13 @@
     <form class="login-form" @submit.prevent>
         <div class="login-form__login">
             <label for="">
-                Login
+               <p>Login</p>
                 <input type="text" name="login" v-model="credentials.login" >
             </label>
         </div>
         <div class="login-form__password">
             <label for="">
-                Paswword
+                <p>Password</p>
                 <input type="text" name="paswword" v-model="credentials.password">
             </label>
         </div>
@@ -49,23 +49,51 @@ import { mapActions } from 'vuex'
 </script>
 <style lang="scss">
     .login-form {
+        margin-top: 100px;
         display: flex;
         flex-direction: column;
         align-items: center;
-        border: 1px solid red;
+        border: 1px solid #9da3b5;
+        /*background: papayawhip;*/
+        padding: 15px;
+
         &__login, &__password {
+            margin: 10px;
             width: 100%;
+            /*color: #9da3b5;*/
         }
         &__button-group {
             display: flex;
-            flex-direction: column;
-            width: 100px;
-            margin-top: 10px;
+            justify-content: center;
+
+
+            margin-top: 15px;
         }
+        &__button-group button{
+            margin-left: 10px;
+            margin-right: 10px;
+            width: 80px;
+            height: 25px;
+            font-size: 15px;
+            cursor: pointer;
+            /*color: #9da3b5;*/
+        }
+
         label {
             display: flex;
             justify-content: space-between;
             width: 100%;
+            height: 30px;
+        }
+        label p{
+            margin: 0 auto;
+            line-height: 30px;
+        }
+        input{
+            margin-right: 10px;
+            font-size: 15px;
+            /*color: #9da3b5;*/
+
         }
     }
 </style>
