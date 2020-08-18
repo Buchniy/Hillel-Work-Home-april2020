42b5669edd29159531e5d967f310f818ea1964bd
Buch <hhccuubb@ukr.net>
2020-08-18 23:25:05 +0300

    24+filter

--- wh24+доп/components.js
+++ wh24+доп/components.js
@@ -14,48 +14,28 @@ Vue.component('Diagram', {
         }
     },
 
-  //   mounted() {
-  //       this.diagramItems[0].height = localStorage.getItem(this.diagramItems);
-  // console.log('localStorage', localStorage);
-  //
-  //   }
-  //   ,
-
-    methods: {
-        currentValue(curHeigh, curName, curObj){
-            console.log('curHeigh, curName, curObj', curHeigh, curName, curObj);
-        },
+    methods:{
         change(){
-            this.diagramItems.sort(function (a, b) {
+          return this.diagramItems.sort(function (a, b) {
                 return a.height - b.height
             });
-
-            console.log('diagramItems',  this.diagramItems);
-            console.log('diagramItems',  this.diagramItems[0].height);
-
         }
     },
+
     template: `
 <div>
     <div class="parent-box">
     
         <diagram-column 
-            :currentColumn="item"
-            
-            @newValue="currentValue"
-
-            v-for="item in diagramItems" /> 
-
+            :currentColumn="item"          
+            v-for="item in diagramItems" :key="item.height"/> 
     </div> 
-
     <hr>
-        <assort 
-  
-        @changeMass="change" />
+        <assort @changeMass="change" />
     </div>   
     `
 });
-//-----------------------------------------///
+
 Vue.component('diagram-column', {
     props: ['currentColumn'],
     data() {
@@ -65,41 +45,28 @@ Vue.component('diagram-column', {
     },
     mounted() {
         this.valueHeight = localStorage.getItem(this.currentColumn.name);
-
-
         this.currentColumn.height = this.valueHeight;
-
-        // console.log('this.currentColumn.heigh', this.currentColumn.heigh);
-
     },
     methods: {
         changeValueHeight(currentValue) {
             this.valueHeight = currentValue ;
             this.currentColumn.height = this.valueHeight;
-            console.log(currentValue, this.valueHeight, this.currentColumn.height, 'currentValue, this.valueHeight, this.currentColumn.heigh' );
-
-            this.$emit('newValue', currentValue, this.currentColumn.name, this.currentColumn);
-
-
-            console.log(currentValue, this.currentColumn.name, this.currentColumn,  'diagram-column');
-            console.log('this.valueHeight', this.valueHeight, this.currentColumn);
         }
     },
     template: `
         <div class="box" >
             <div class="color-box" :style="{background: currentColumn.color, height: valueHeight + 'px'}">
             </div>
-            <div>{{currentColumn.name}} : {{currentColumn.height}}</div>
+            <div>{{currentColumn.name}}</div>
             <regulator 
-            :valueHei="currentColumn.height"
             :valueRange="currentColumn.name" 
                 @changeValue="changeValueHeight"/>
         </div>
     `
 });
-//-----------------------------------------///
+
 Vue.component('regulator', {
-    props: ['valueRange', 'valueHei'],
+    props: ['valueRange'],
     data() {
         return {
             value: 0,
@@ -108,6 +75,7 @@ Vue.component('regulator', {
     mounted() {
 
         let getStor = localStorage.getItem(this.valueRange);
+
         if (getStor) {
             this.value = getStor;
         } else {
@@ -117,38 +85,24 @@ Vue.component('regulator', {
 
     methods: {
         onClick(event) {
-
             this.$emit('changeValue', this.value);
             localStorage.setItem(this.valueRange, this.value);
-            console.log(this.valueRange, this.value, 'regulator');
         }
     },
     template: `
         <div>
             <input type="range" v-model="value" @input="onClick"  max="200">
             <hr>
-            {{valueHei}} -- 
             {{value}}
         </div>
     `
 });
-//-----------------------------------------///
+
 Vue.component('assort', {
 
-    data() {
-        return {
-
-        }
-    },
-    mounted() {
-
-
-    },
     methods: {
         assort(){
-            // console.log('arr');
             this.$emit('changeMass');
-
         }
     },
     template: `
@@ -157,7 +111,7 @@ Vue.component('assort', {
         </div>
     `
 });
-//-----------------------------------------///
+
 const vue = new Vue({
     el: '#app',
     data: {}
